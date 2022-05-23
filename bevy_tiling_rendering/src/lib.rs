326c06ef6a97b8b7ee44adde581fb16737cb0148
James <jamescarterbell@gmail.com>
2022-05-23 19:38:26 -0400

    fixed cache checking issue

--- bevy_tiling_rendering/src/lib.rs
+++ bevy_tiling_rendering/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::{Deref, DerefMut};
+
 use bevy::{
     math::IVec3,
     prelude::{Commands, Component, Entity, Plugin, Query, Res, ResMut, Transform, With, Without},
@@ -14,13 +16,15 @@ pub struct TilingRenderPlugin;
 
 impl Plugin for TilingRenderPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_system_to_stage(TilingCoreStage::Update, add_render_entitites);
         let render_app = app.sub_app_mut(RenderApp);
-        render_app.add_system_to_stage(RenderStage::Extract, extract);
+        render_app
+            .init_resource::<TilingCache>()
+            .add_system_to_stage(RenderStage::Cleanup, cache_tile_rendering_entities)
+            .add_system_to_stage(RenderStage::Extract, extract);
     }
 }
 
-#[derive(Component)]
+#[derive(Component, Clone)]
 pub enum TilingBuffer {
     Unloaded,
     Unmeshed(Buffer),
@@ -31,22 +35,27 @@ pub enum TilingBuffer {
     },
 }
 
-#[derive(Component)]
-/// Placed on gameplay world components to point towards
-/// rendering world components.
-pub struct RenderEntity(Option<Entity>);
-
-#[derive(Component)]
+#[derive(Component, Clone)]
 /// Placed on rendering world components to point
 /// towards gameplay world chunks.
 pub struct RenderKey(IVec3);
 
-fn add_render_entitites(
-    mut commands: Commands,
-    chunks: Query<Entity, (With<ChunkMarker>, Without<RenderEntity>)>,
-) {
-    for chunk in chunks.iter() {
-        commands.entity(chunk).insert(RenderEntity(None));
+#[derive(Default)]
+struct TilingCache {
+    cache: Vec<(Entity, (TilingBuffer, RenderKey))>,
+}
+
+impl Deref for TilingCache {
+    type Target = Vec<(Entity, (TilingBuffer, RenderKey))>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.cache
+    }
+}
+
+impl DerefMut for TilingCache {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.cache
     }
 }
 
@@ -54,35 +63,37 @@ fn extract(
     mut commands: Commands,
     mut tilemap_writer: TileMapWriter,
     chunk_map: Res<ChunkMap>,
-    mut chunks: Query<(Entity, &Transform, &mut RenderEntity), With<ChunkMarker>>,
+    mut chunks: Query<(Entity, &Transform), With<ChunkMarker>>,
     mut render_world: ResMut<RenderWorld>,
 ) {
-    // Remove the render device so we can perform other borrows from the render_world
-    let render_device = render_world
-        .remove_resource::<RenderDevice>()
-        .expect("Couldn't find RenderDevice");
-
-    let mut rendering_entity_query = render_world.query::<(Entity, &TilingBuffer, &RenderKey)>();
-
-    for (entity, buffer, key) in rendering_entity_query.iter(&render_world) {
-        // Make sure this chunk still exists
-        if tilemap_writer.get_chunk(&key.0).is_some() {
-            // Check if the chunk hasn't been updated (since we handle that from gameplay world side)
-            if !tilemap_writer.is_chunk_updated(&key.0) {
-                if let TilingBuffer::Unloaded = buffer {
-                    // Check if this chunk will be in the camera view this frame, if it will, we should just update it
-                    // TODO: CHECK IF IN FRAME HERE, WE NEED TO DO IT DURING EXTRACT WHILE WE HAVE ALL BUFFERS
-                    tilemap_writer.mark_chunk_updated(&key.0);
+    if let Some(mut cache) = render_world.get_resource_mut::<TilingCache>() {
+        for (_, (buffer, key)) in cache.iter() {
+            // Make sure this chunk still exists
+            if tilemap_writer.get_chunk(&key.0).is_some() {
+                // Check if the chunk hasn't been updated (since we handle that from gameplay world side)
+                if !tilemap_writer.is_chunk_updated(&key.0) {
+                    if let TilingBuffer::Unloaded = buffer {
+                        // Check if this chunk will be in the camera view this frame, if it will, we should just update it
+                        // TODO: CHECK IF IN FRAME HERE, WE NEED TO DO IT DURING EXTRACT WHILE WE HAVE ALL BUFFERS
+                        tilemap_writer.mark_chunk_updated(&key.0);
+                    }
                 }
             }
         }
+        let old_cache = std::mem::take(&mut cache.cache);
+        commands.insert_or_spawn_batch(old_cache);
     }
 
-    // If a chunk has been updated, we want to refresh it's tile buffer
-    for (ent, transform, mut render_ent) in chunks.iter_mut() {
+    let render_device = render_world
+        .get_resource_mut::<RenderDevice>()
+        .expect("Couldn't find RenderDevice");
+
+    for (ent, transform) in chunks.iter_mut() {
         let chunk_key = chunk_map
             .get_chunk_index(&ent)
             .expect("Couldn't find chunk in map");
+
+        // If a chunk has been updated, we want to refresh it's tile buffer
         if tilemap_writer.is_chunk_updated(chunk_key) {
             let buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
                 label: Some("raw_tile_buffer"),
@@ -94,18 +105,73 @@ fn extract(
             });
 
             commands
-                .entity(ent)
+                .get_or_spawn(ent)
                 .insert(TilingBuffer::Unmeshed(buffer))
                 .insert(RenderKey(*chunk_key));
+
+            println!("Hello");
         }
 
-        commands.entity(ent).insert(*transform);
+        commands.get_or_spawn(ent).insert(*transform);
     }
-
-    // Reinsert the render device
-    render_world.insert_resource(render_device);
 }
 
 fn prepare() {}
 
-fn cleanup() {}
+fn cache_tile_rendering_entities(
+    mut tiling_cache: ResMut<TilingCache>,
+    render_chunks: Query<(Entity, &TilingBuffer, &RenderKey)>,
+) {
+    for (entity, buffer, key) in render_chunks.iter() {
+        tiling_cache.push((entity, (buffer.clone(), key.clone())));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bevy::{math::IVec3, prelude::App, render::RenderApp, DefaultPlugins};
+    use bevy_tiling_chunk_ecs::TilingChunkEcsPlugin;
+    use bevy_tiling_core::{Tile, TileCoord, TileMapWriter, TilingCorePlugin};
+
+    use crate::{RenderKey, TilingRenderPlugin};
+
+    use crate::TilingCache;
+
+    #[test]
+    fn caches_tiles() {
+        // place 4 tiles in different chunks
+        let mut app = App::new();
+        app.add_plugins(DefaultPlugins)
+            .add_plugin(TilingCorePlugin)
+            .add_plugin(TilingChunkEcsPlugin)
+            .add_plugin(TilingRenderPlugin);
+
+        app.add_system(add_4_tile);
+
+        app.update();
+
+        let render_app = app.sub_app_mut(RenderApp);
+        let cache = render_app.world.get_resource::<TilingCache>().unwrap();
+
+        assert_eq!(cache.len(), 4);
+    }
+
+    fn add_4_tile(mut tilemap_writer: TileMapWriter) {
+        tilemap_writer.set_tile(
+            &TileCoord::new(IVec3::from((0, 0, 0)), 0),
+            Some(Tile::new(0, 0)),
+        );
+        tilemap_writer.set_tile(
+            &TileCoord::new(IVec3::from((1, 0, 0)), 0),
+            Some(Tile::new(0, 0)),
+        );
+        tilemap_writer.set_tile(
+            &TileCoord::new(IVec3::from((2, 0, 0)), 0),
+            Some(Tile::new(0, 0)),
+        );
+        tilemap_writer.set_tile(
+            &TileCoord::new(IVec3::from((3, 0, 0)), 0),
+            Some(Tile::new(0, 0)),
+        );
+    }
+}
